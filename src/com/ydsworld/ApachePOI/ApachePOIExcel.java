44ffe683a3d1bc0b7cfe836c7cf69acbed42753a
ydsworld <support@ydsworld.com>
2015-05-15 22:43:35 -0500

    from command line

--- src/com/ydsworld/ApachePOI/ApachePOIExcel.java
+++ src/com/ydsworld/ApachePOI/ApachePOIExcel.java
@@ -4,11 +4,16 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.lang.invoke.SwitchPoint;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DateUtil;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -17,35 +22,61 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ApachePOIExcel {
 
-	static XSSFRow row;
+	static Row row;
 	   public static void main(String[] args) throws Exception 
 	   {
-	      FileInputStream fis = new FileInputStream(
-	      new File("c:\\class\\chart.xlsx"));
+	      
+		  List<String> list = new ArrayList<String>();
+		  FileInputStream fis = new FileInputStream(
+	      new File("C:\\Users\\YDSWorld\\workspace\\Tutorial\\src\\Book1.xlsx"));
 	      XSSFWorkbook workbook = new XSSFWorkbook(fis);
 	      XSSFSheet spreadsheet = workbook.getSheetAt(0);
 	      Iterator < Row > rowIterator = spreadsheet.iterator();
+	      final StringBuilder b = new StringBuilder();
+	      SimpleDateFormat sdf = new SimpleDateFormat("MM/dd");
+	      
 	      while (rowIterator.hasNext()) 
 	      {
-	         row = (XSSFRow) rowIterator.next();
-	         Iterator < Cell > cellIterator = row.cellIterator();
-	         while ( cellIterator.hasNext()) 
-	         {
-	            Cell cell = cellIterator.next();
-	            switch (cell.getCellType()) 
+	    	  
+	         row = (Row) rowIterator.next();
+	         Cell cell =  row.getCell(0);
+	        
+	         b.append(cell.toString()+",");
+	        // list.add(cell.toString());
+	         
+	         switch (cell.getCellType()) 
 	            {
 	               case Cell.CELL_TYPE_NUMERIC:
-	               System.out.print( 
-	               cell.getNumericCellValue() + " \t\t " );
-	               break;
+	            	   if (DateUtil.isCellDateFormatted(cell)) {
+	            		   try {
+
+	            			    list.add(sdf.format(cell.getDateCellValue()));
+	            		//	    System.out.println(sdf.format(cell.getDateCellValue()));
+
+	            			    } catch (Exception e) {
+	            			            e.printStackTrace();
+	            			    }
+	                    } else {
+	                //        System.out.println(cell.getNumericCellValue() + " \t\t " );
+	                        
+	                    }
+	                    break;
 	               case Cell.CELL_TYPE_STRING:
-	               System.out.print(
-	               cell.getStringCellValue() + " \t\t " );
-	               break;
+	            	   list.add(cell.getStringCellValue());
+	              // System.out.print(cell.getStringCellValue() + " \t\t " );
+	               
 	            }
-	         }
-	         System.out.println();
-	      }
+	         
+	      }//white
 	      fis.close();
-	   }		
+	      
+	      for(int i=0; i<list.size();i++){
+	    	  System.out.println(list.get(i));
+		  }
+	      
+	      //System.out.println(b.toString());
+	   }
+	   
+	   
+  
 }
